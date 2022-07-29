bbb91e7d4d143515bcb6bc15f6a1231d234ba4ae
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2022-07-29 21:45:27 +0700

    tambah layouts template

---
+++ resources/views/dashboard/layouts/head.blade.php
@@ -0,0 +1,14 @@
+<!-- plugins:css -->
+<link rel="stylesheet" href="{{ asset('theme/template/vendors/feather/feather.css') }}">
+<link rel="stylesheet" href="{{ asset('theme/template/vendors/ti-icons/css/themify-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('theme/template/vendors/css/vendor.bundle.base.css') }}">
+<!-- endinject -->
+<!-- Plugin css for this page -->
+<link rel="stylesheet" href="{{ asset('theme/template/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+<link rel="stylesheet" href="{{ asset('theme/template/vendors/ti-icons/css/themify-icons.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('theme/template/js/select.dataTables.min.css') }}">
+<!-- End plugin css for this page -->
+<!-- inject:css -->
+<link rel="stylesheet" href="{{ asset('theme/template/css/vertical-layout-light/style.css') }}">
+<!-- endinject -->
+<link rel="shortcut icon" href="{{ asset('theme/template/images/favicon.png') }}" />
---
+++ resources/views/dashboard/layouts/scripts.blade.php
@@ -0,0 +1,21 @@
+<!-- plugins:js -->
+<script src="{{ asset('theme/template/vendors/js/vendor.bundle.base.js') }}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{ asset('theme/template/vendors/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('theme/template/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+<script src="{{ asset('theme/template/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
+<script src="{{ asset('theme/template/js/dataTables.select.min.js') }}"></script>
+
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{ asset('theme/template/js/off-canvas.js') }}"></script>
+<script src="{{ asset('theme/template/js/hoverable-collapse.js') }}"></script>
+<script src="{{ asset('theme/template/js/template.js') }}"></script>
+<script src="{{ asset('theme/template/js/settings.js') }}"></script>
+<script src="{{ asset('theme/template/js/todolist.js') }}"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="{{ asset('theme/template/js/dashboard.js') }}"></script>
+<script src="{{ asset('theme/template/js/Chart.roundedBarCharts.js') }}"></script>
+<!-- End custom js for this page-->
