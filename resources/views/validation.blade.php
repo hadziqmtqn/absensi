d77a60dd5ad6fca376454652bf10578f885d8605
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2022-07-30 15:39:42 +0700

    update register

--- resources/views/validation.blade.php
+++ resources/views/validation.blade.php
@@ -1,11 +1,6 @@
 <script>
-<<<<<<< HEAD
     if ($("#register").length > 0) {
         $("#register").validate({
-=======
-    if ($("#registration").length > 0) {
-        $("#registration").validate({
->>>>>>> e23ff7b57be36abfdc49b7bf2b1162301b12243b
             rules: {
                 name: {
                     required: true,
@@ -16,11 +11,7 @@
                 nik: {
                     minlength: 16,
                     maxlength: 16,
-<<<<<<< HEAD
                 },
-=======
-                }
->>>>>>> e23ff7b57be36abfdc49b7bf2b1162301b12243b
                 phone: {
                     required: true,
                     minlength: 11,
@@ -51,10 +42,7 @@
                     maxlength: "NIK Harus 16 digit",
                 },
                 phone: {
-<<<<<<< HEAD
                     required: "No. Telp/HP Wajib diisi",
-=======
->>>>>>> e23ff7b57be36abfdc49b7bf2b1162301b12243b
                     number: "No. Telp/HP Harus Berupa Angka",
                     minlength: "No. Telp/HP Harus Lebih dari 11 Digit",
                     maxlength: "No. Telp/HP Harus Kurang dari 13 Digit",
